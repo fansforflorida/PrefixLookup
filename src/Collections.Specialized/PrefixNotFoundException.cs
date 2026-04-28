1db2b19ecbc75d7f637b5548dd6dd8d2c209b3a1
John Hall <jhall@alum.wpi.edu>
2026-04-27 22:05:06 -0400

    Use primary constructor

--- src/Collections.Specialized/PrefixNotFoundException.cs
+++ src/Collections.Specialized/PrefixNotFoundException.cs
@@ -3,14 +3,10 @@ namespace Collections.Specialized;
 /// <summary>
 /// Exception thrown when a prefix is not found in a prefix lookup.
 /// </summary>
-public class PrefixNotFoundException : KeyNotFoundException
+/// <remarks>
+/// Initializes a new instance of the <see cref="PrefixNotFoundException"/> class with a specified error message.
+/// </remarks>
+/// <param name="message">The message that describes the error.</param>
+public class PrefixNotFoundException(string? message) : KeyNotFoundException(message)
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="PrefixNotFoundException"/> class with a specified error message.
-    /// </summary>
-    /// <param name="message">The message that describes the error.</param>
-    public PrefixNotFoundException(string? message)
-        : base(message)
-    {
-    }
 }
